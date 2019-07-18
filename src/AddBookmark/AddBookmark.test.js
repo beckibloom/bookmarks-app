401c3b4876c1dff234331bad5c78902d848bfcfa
Becki Blumer <rblumer13@gmail.com>
2019-07-18 11:44:37 -0500

    updated AddBookmark.test.js to accommodate route path

--- src/AddBookmark/AddBookmark.test.js
+++ src/AddBookmark/AddBookmark.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom';
 import AddBookmark from './AddBookmark';
 
 it('renders without crashing', () => {
+  const addBookmarkRender = withRouter(<AddBookmark />)
   const div = document.createElement('div');
-  ReactDOM.render(<AddBookmark />, div);
+  ReactDOM.render(addBookmarkRender, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$t.MlNYfCqfrgQZPFKlR8EOEc6izkUF6vf6FqCIB0zCTGCrxPBerae',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
